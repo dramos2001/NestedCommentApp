c76b99f2146a389ce888f16a317ada6412592a14
dramos2001 <damianramos00@gmail.com>
2023-05-11 20:36:24 -0700

    Can make new post and reply

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const Comment = ({ comment }) => {
+  const [replyName, setReplyName] = useState('');
+  const [replyText, setReplyText] = useState('');
+  const [replies, setReplies] = useState([]);
+
+  const handleReplyChange = (e) => {
+    setReplyText(e.target.value);
+  };
+
+  const handleReplyNameChange = (e) => {
+    setReplyName(e.target.value);
+  };
+
+  const handleReplySubmit = (e, name) => {
+    e.preventDefault();
+    const newReply = {
+      name: name,
+      text: replyText,
+      replies: []
+    };
+    setReplies([...replies, newReply]);
+    setReplyText('');
+    setReplyName('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='comment'>
+      <p>{comment.name}</p>
+      <p>{comment.text}</p>
+      <form onSubmit={(e) => handleReplySubmit(e, replyName)} className='reply-form'>
+        <input type='text' name='replyNameInput' placeholder='Name...' 
+         value={replyName} onChange={handleReplyNameChange}/>
+        <input type='text' placeholder='Reply...' 
+         value={replyText} onChange={handleReplyChange}/>
+        <button type='submit' className='button'>Submit</button>
+      </form>
+      <div className='replies'>
+        {replies.map((reply, index) => (
+          <div className='comment' key={index}>
+            <Comment comment={reply}/>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+function App() {
+  const [nameInput, setNameInput] = useState('');
+  const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState('');
+
+  const handleCommentChange = (e) => {
+    setCommentText(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setNameInput(e.target.value);
+  };
+
+  const handleCommentSubmit = (e, parentCommentIndex, name) => {
+    e.preventDefault();
+    const newComment = {
+      name: name,
+      text: commentText,
+      replies: []
+    };
+
+    if (parentCommentIndex !== undefined) {
+      const updatedComments = [...comments];
+      updatedComments[parentCommentIndex].replies.push(newComment);
+      setComments(updatedComments);
+    } else {
+      setComments([...comments, newComment]);
+    }
+
+    setCommentText('');
+    setNameInput('');
+  };
+
+  return (
+    <div className='App'>
+      <h1>New Post</h1>
+      <form onSubmit={(e) => handleCommentSubmit(e, undefined, nameInput)} className='comment-form'>
+        <input type='text' name='nameInput' placeholder='Name...' 
+         value={nameInput} onChange={handleNameChange}/>
+        <input type='text' name='commentText' placeholder='Write a new post...'
+         value={commentText} onChange={handleCommentChange}/>
+        <button type='submit' className='button'>Submit</button>
+      </form>
+      <div className='comments'>
+        {comments.map((comment, index) => (
+          <div className='comment' key={index}>
+            <Comment comment={comment}/>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
